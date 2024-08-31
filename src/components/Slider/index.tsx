51c34e7c0836e668b765e9dc4dee5b4a4fa67ae5
armen <arakelyan2001@mail.ru>
2024-08-31 13:27:29 +0400

    finish slider animation

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,39 +1,49 @@
-'use client';
-
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import * as S from './Slider.styled';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper modules
 import { Navigation } from 'swiper/modules';
-
-// Import images
+import { ArrowRight } from 'lucide-react';
 import slider from '@/public/images/home/slider/slider.png';
-
 import arrowLeft from '@/public/images/arrow-l.svg';
 import arrowRight from '@/public/images/arrow-r.svg';
 
 const slides = [
   {
     backgroundPosition: '0% 0%',
-    content: 'Slide 1 Content',
+    title: 'VRF Systems',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.',
+    translateX: 10,
   },
   {
     backgroundPosition: '30% 0%',
-    content: 'Slide 2 Content',
+    title: 'R290 M thermal',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.',
+    translateX: 40,
   },
   {
     backgroundPosition: '63% 0%',
-    content: 'Slide 3 Content',
+    title: 'Chiller',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.',
+    translateX: 40,
   },
   {
     backgroundPosition: '100% 0%',
-    content: 'Slide 4 Content',
+    title: 'BreezeleSS+',
+    content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.',
+    translateX: 75,
   },
 ];
 
 function Slider() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [translateX, setTranslateX] = useState(slides[0].translateX);
+
+  const handleSlideChange = (swiper: any) => {
+    setActiveIndex(swiper.activeIndex);
+    setTranslateX(slides[swiper.activeIndex].translateX);
+  };
+
   return (
     <S.SliderWrapper>
       <Swiper
@@ -41,6 +51,7 @@ function Slider() {
         spaceBetween={0}
         slidesPerView={1}
         slidesPerGroup={1}
+        onSlideChange={handleSlideChange}
         navigation={{
           nextEl: '.custom-next',
           prevEl: '.custom-prev',
@@ -52,12 +63,25 @@ function Slider() {
             <S.Slide
               $backgroundImage={slider.src}
               $backgroundPosition={slide.backgroundPosition}
-            >
-              <S.SlideContent>{slide.content}</S.SlideContent>
-            </S.Slide>
+            />
           </SwiperSlide>
         ))}
       </Swiper>
+      {slides.map((slide, index) => (
+        <S.SlideContentWrapper
+          key={index}
+          translateX={translateX}
+          isActive={activeIndex === index}
+        >
+          <S.SlideTitle>{slide.title}</S.SlideTitle>
+          <S.SlideContent>{slide.content}</S.SlideContent>
+          <S.ButtonWrapper>
+            <S.CalculatorButton>
+              Go to calculator <ArrowRight size={18} />
+            </S.CalculatorButton>
+          </S.ButtonWrapper>
+        </S.SlideContentWrapper>
+      ))}
       <S.CustomNavigationButton
         className="custom-prev"
         $left={true}
