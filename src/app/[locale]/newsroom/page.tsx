268a35cdacb2366fc87a63ff5708e48f12fba444
arthurmkhitaryan <arthurmkhitaryan>
2024-12-24 00:20:07 +0400

    change fix

--- src/app/[locale]/newsroom/page.tsx
+++ src/app/[locale]/newsroom/page.tsx
@@ -2,7 +2,10 @@
 
 import React, { useState } from 'react';
 import { useTranslations } from 'next-intl';
-import { useGetNewsQuery, useGetNewsWithBannerQuery } from '@/features/newsroom/newsroom.api';
+import {
+  useGetNewsQuery,
+  useGetNewsWithBannerQuery,
+} from '@/features/newsroom/newsroom.api';
 import MainLayout from '@/components/Layout';
 import { getImageUrl } from '@/utils/getImageFullUrl';
 import NewsroomItem from '@/components/NewsRoomItem';
@@ -21,11 +24,18 @@ export default function Newsroom() {
   const [currentPage, setCurrentPage] = useState(1);
   const [activeCategory, setActiveCategory] = useState(0);
 
-  const { data: newsData, isLoading: isLoadingNewsRooms } = useGetNewsQuery({ locale: locale as string, page: currentPage, pageSize: PAGE_SIZE, category: activeCategory !== 0 ? activeCategory : undefined });
-  
-  const { data: bannerPost, isLoading: isLoadingBannerPost } = useGetNewsWithBannerQuery({ locale: locale as string });
+  const { data: newsData, isLoading: isLoadingNewsRooms } = useGetNewsQuery({
+    locale: locale as string,
+    page: currentPage,
+    pageSize: PAGE_SIZE,
+    category: activeCategory !== 0 ? activeCategory : undefined,
+  });
 
-  const { data: categories, isLoading: isLoadingCategories } = useGetNewsCategoriesQuery({ locale: locale as string });
+  const { data: bannerPost, isLoading: isLoadingBannerPost } =
+    useGetNewsWithBannerQuery({ locale: locale as string });
+
+  const { data: categories, isLoading: isLoadingCategories } =
+    useGetNewsCategoriesQuery({ locale: locale as string });
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -35,12 +45,13 @@ export default function Newsroom() {
     setActiveCategory(activeCategory);
     setCurrentPage(1);
   };
-
   const formattedDate = new Intl.DateTimeFormat('en-GB', {
     day: '2-digit',
     month: '2-digit',
-    year: 'numeric'
-  }).format(new Date(bannerPost ? bannerPost?.createdAt : new Date())).replaceAll('/', '.');
+    year: 'numeric',
+  })
+    .format(new Date(bannerPost?.createdAt ?? new Date()))
+    .replaceAll('/', '.');
 
   if (isLoadingNewsRooms || isLoadingBannerPost) return <div>Loading...</div>;
 
@@ -52,25 +63,56 @@ export default function Newsroom() {
             <S.BannerImage src={getImageUrl(bannerPost?.smallImage)} />
             <S.BannerContent>
               <S.BannerTitle>{bannerPost?.title}</S.BannerTitle>
-              <S.BannerDescription>{bannerPost?.smallDescription}</S.BannerDescription>
-              <S.PostAuthor>{bannerPost?.author} | {formattedDate}</S.PostAuthor>
+              <S.BannerDescription>
+                {bannerPost?.smallDescription}
+              </S.BannerDescription>
+              <S.PostAuthor>
+                {bannerPost?.author} | {formattedDate}
+              </S.PostAuthor>
               <S.ReadFullButtonWrapper>
-                <S.ReadFullButton>{t('read_full_story')} {">>"}</S.ReadFullButton>
+                <S.ReadFullButton>
+                  {t('read_full_story')} {'>>'}
+                </S.ReadFullButton>
               </S.ReadFullButtonWrapper>
             </S.BannerContent>
           </S.BannerWrapper>
         )}
         <S.Tabs>
-          <S.Tab active={activeCategory === 0} key={'all'} onClick={() => handleCategoryChange(0)}>All Topics</S.Tab>
-          {!isLoadingCategories && categories?.map((item) => (
-            <S.Tab active={activeCategory === item.id} onClick={() => handleCategoryChange(item.id)} key={item.id}>{item.name}</S.Tab>
-          ))}
+          <S.Tab
+            active={activeCategory === 0}
+            key={'all'}
+            onClick={() => handleCategoryChange(0)}
+          >
+            All Topics
+          </S.Tab>
+          {!isLoadingCategories &&
+            categories?.map((item) => (
+              <S.Tab
+                active={activeCategory === item.id}
+                onClick={() => handleCategoryChange(item.id)}
+                key={item.id}
+              >
+                {item.name}
+              </S.Tab>
+            ))}
         </S.Tabs>
-        {newsData?.data?.map(itm => (
-          <NewsroomItem key={itm.id} id={itm.id} author={itm.author} image={itm.smallImage} smallDescription={itm.smallDescription} title={itm.title} date={itm.createdAt} />
+        {newsData?.data?.map((itm) => (
+          <NewsroomItem
+            key={itm.id}
+            id={itm.id}
+            author={itm.author}
+            image={itm.smallImage}
+            smallDescription={itm.smallDescription}
+            title={itm.title}
+            date={itm.createdAt || new Date()}
+          />
         ))}
         <S.PaginationWrapper>
-          <Pagination currentPage={currentPage} onPageChange={handlePageChange} totalPages={Math.ceil((newsData?.totalCount || 0) / PAGE_SIZE)} />
+          <Pagination
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+            totalPages={Math.ceil((newsData?.totalCount || 0) / PAGE_SIZE)}
+          />
         </S.PaginationWrapper>
         <ProductList />
       </MainLayout>
